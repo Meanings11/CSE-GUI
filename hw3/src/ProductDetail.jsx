151239afb298c915e1edfb7173ed77f293b1341b
Meanings11 <mingful@smu.edu>
2019-04-05 22:43:30 -0500

    productDetail done

--- hw3/src/ProductDetail.jsx
+++ hw3/src/ProductDetail.jsx
@@ -3,29 +3,39 @@ import {Product} from './models/Product';
 import {ProductReview} from './models/ProductReview';
 
 export class ProductDetail extends React.Component{
-    products = [
-        new Product(
-
-        )
-    ];
 
     state = {
-
+        product:new Product(
+            1,
+            "Jif Peanut, 40 ounce",
+            "7g of Protein per serving with no preservatives, artifical flavors or colors.", 
+            7.99,
+            "http://johnlawrimore.com/smu/101.png",
+            []
+        )   
     };
 
     render(){
         return (
+            <main>
             <nav aria-label="breadcrumb">
                 <ol className="breadcrumb">
                     <li className="breadcrumb-item"><a href="#">Tasty snacks</a></li>
-                    <li className="breadcrumb-item active" aria-current="page">ProductName</li>  
+                    <li className="breadcrumb-item active" aria-current="page">{this.state.product.name}</li>  
                 </ol>
             </nav>
 
-            <div className="jumbotron">
-                <h1>Name</h1>
+            <div className ="jumbotron">
+                
+                <div className = "float-left product-img mr-4 ml-1">  
+                    <img src = {this.state.product.imageUrl} width="200" height="300"/>
+                </div>
 
+                <h1>{this.state.product.name}</h1>
+                <h3><span class="badge badge-lg badge-primary">{this.state.product.price}</span></h3>
+                <p>{this.state.product.description}</p>
             </div>
+            </main>
         );
     }
 }
